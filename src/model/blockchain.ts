ffd7d7c7a30fe76afbd08fdca281aec5089c09c8
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-03 13:31:24 -0300

    validate blockchain hashes

--- src/model/blockchain.ts
+++ src/model/blockchain.ts
@@ -1,3 +1,5 @@
+import { threadId } from "worker_threads";
+import { cryptoHash } from "../utils/crytoHash";
 import { Block } from "./block";
 
 export class Blockchain {
@@ -10,6 +12,35 @@ export class Blockchain {
     const lastBlock = this._chain[this.len() - 1];
     const block = Block.mine({ lastBlock, data });
     this._chain.push(block);
+  }
+
+  static isValidChain(chain: Block[]): boolean {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+      return false;
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, hash, lastHash, data } = chain[i];
+      const previusHash = chain[i - 1].hash;
+      if (previusHash !== lastHash) return false;
+      const validateHash = cryptoHash(timestamp, lastHash, data);
+      if (hash !== validateHash) return false;
+    }
+    return true;
+  }
+
+  setFieldAt({
+    index,
+    field,
+    data,
+  }: {
+    index: number;
+    field: string;
+    data: any;
+  }) {
+    this._chain[index] = { ...this._chain[index], [field]: data } as Block;
+  }
+
+  setBlockAt(index: number, block: Block) {
+    this._chain[index] = block;
   }
 
   getChainByIndex(index: number): Block {
--- tests/blockchain.test.ts
+++ tests/blockchain.test.ts
@@ -4,8 +4,12 @@ import { GENESIS_DATA } from "../src/config/config";
 import { IBlockProps } from "../src/interfaces/block/IBlock";
 
 describe("Blockchain", () => {
-  const blockchain = new Blockchain();
-  const genesisBlock = Block.genesis();
+  let blockchain: Blockchain;
+  let genesisBlock: Block;
+  beforeEach(() => {
+    blockchain = new Blockchain();
+    genesisBlock = Block.genesis();
+  });
   it("contains a chain array instance", () => {
     expect(blockchain.chain instanceof Array).toBe(true);
   });
@@ -17,5 +21,51 @@ describe("Blockchain", () => {
     blockchain.addBlock({ data });
     const len: number = blockchain.len() - 1;
     expect(blockchain.getChainByIndex(len).data).toEqual(data);
+  });
+
+  describe("isValidChain", () => {
+    describe("when the chain does not start with the genesis block", () => {
+      it("returns false", () => {
+        const fakeData = "fake-genesis";
+        blockchain.setFieldAt({ index: 0, field: "_data", data: fakeData });
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe("when the chain starts with the genesis block and has multiple blocks", () => {
+      describe("and the lastHash reference has changed", () => {
+        it("returns false", () => {
+          blockchain.addBlock({ data: "Web3" });
+          blockchain.addBlock({ data: "Ethereum" });
+          blockchain.addBlock({ data: "Solidity" });
+          blockchain.setFieldAt({
+            index: 2,
+            field: "_lastHash",
+            data: "fake-hash",
+          });
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+    });
+
+    describe("and the chain contains a block with invalid field", () => {
+      it("returns false", () => {
+        blockchain.addBlock({ data: "Web3" });
+        blockchain.addBlock({ data: "Ethereum" });
+        blockchain.addBlock({ data: "Solidity" });
+        blockchain.setFieldAt({
+          index: 2,
+          field: "_data",
+          data: "evil-data",
+        });
+
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe("and the chain not contains any invalid blocks", () => {
+      it("returns true", () => {});
+    });
   });
 });
